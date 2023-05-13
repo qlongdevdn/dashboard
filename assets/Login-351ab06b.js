e37dbdc717d8383dcbb6716a6ff9576249d543ab
qlongdevdn <qlongdevdn@users.noreply.github.com>
2023-05-13 06:29:34 +0000

    deploy: 70884c7aba17541496811bfa46a90030cdc77849

---
+++ assets/Login-351ab06b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-9aeb4376.js";const n=()=>r.jsx(r.Fragment,{children:"Login"});export{n as default};
---
+++ assets/index-ea34d8ce.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-9aeb4376.js";const r=()=>e.jsx("div",{className:"page",children:e.jsx("h1",{children:"Hello Dashboard"})});export{r as default};
